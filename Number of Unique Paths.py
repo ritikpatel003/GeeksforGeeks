7f218452d21ab0308033a588b34044faad76896f
ritikpatel003 <1804633@kiit.ac.in>
2021-02-06 23:43:53 +0530

    Number of Unique Paths.py

--- Number of Unique Paths.py
+++ Number of Unique Paths.py
@@ -1,13 +1,7 @@
 a=3
-b=2
-dp=[]
-for i in range(b+1):
-	dp.append((a+1)*[0])
-for i in range(a+1):
-	dp[0][i]=1
-for i in range(b+1):
-	dp[i][0]=1
-for i in range(1,b+1):
-	for j in range(1,a+1):
-		dp[i][j]=dp[i-1][j]+dp[i][j-1]
-print(dp[b][a])
+b=4
+dp=[[1 for i in range(a)] for j in range(b)]
+for j in range(b-2,-1,-1):
+	for i in range(a-2,-1,-1):
+		dp[j][i]=dp[j+1][i]+dp[j][i+1]
+print(dp[0][0])
